e7c3a099bcdac99f7a977a16e3a13d32f709f1ee
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-10-01 03:22:14 +0900

    [add] Pageコンポーネントの作成

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -1,20 +1,95 @@
-import {NextPage} from "next";
-import {FC} from "react";
-import {Head} from "~/components/ui/Head";
+import { NextPage } from 'next'
+import type { FC } from 'react'
+import Image from 'next/image'
+import { Box, Container } from '@mui/system'
+import { TextField, Typography } from '@mui/material'
+import { Button } from '~/components/ui/Button'
+
+import { Head } from '~/components/ui/Head'
 
 const Login: FC = () => {
-    return (
-        <p>ログインページ</p>
-    )
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '80vh',
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        width="100%"
+      >
+        <Box component="div">
+          <Image src="/logo.png" width={200} height={150} objectFit="contain" />
+        </Box>
+        <Typography variant="h5" marginBottom="28px">
+          管理画面
+        </Typography>
+
+        <Box
+          component="section"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap="16px"
+          width={'100%'}
+          marginBottom="16px"
+        >
+          <Typography variant="h6" marginBottom="16px">
+            ログイン
+          </Typography>
+          <Box
+            component="form"
+            width="100%"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap="16px"
+          >
+            <TextField
+              id="email"
+              type="email"
+              label="メールアドレス"
+              InputLabelProps={{ shrink: true }}
+              required={true}
+              fullWidth={true}
+            />
+            <TextField
+              id="password"
+              type="password"
+              label="パスワード"
+              InputLabelProps={{ shrink: true }}
+              required={true}
+              fullWidth={true}
+            />
+          </Box>
+        </Box>
+        <Button
+          color="primary"
+          variant={'contained'}
+          size="large"
+          disabled={false}
+          loading={false}
+        >
+          ログイン
+        </Button>
+      </Box>
+    </Container>
+  )
 }
 
-const Index: NextPage = () =>  {
-    return(
-        <>
-            <Head title='ログイン'/>
-            <Login/>
-        </>
-    )
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head title="ログイン" />
+      <Login />
+    </>
+  )
 }
 
 export default Index
