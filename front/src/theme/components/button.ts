c6e63b1ddc3bd46a5afc6d3becce90c54a603cd8
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-10-02 14:34:23 +0900

    [update] カスタムテーマのアップデート

--- front/src/theme/components/button.ts
+++ front/src/theme/components/button.ts
@@ -1,30 +1,34 @@
+import { blue, grey } from '@mui/material/colors'
 import type { Components } from '@mui/material'
-import { blue } from '@mui/material/colors'
 
 export const MuiButton: Components['MuiButton'] = {
   styleOverrides: {
     // primary
-    containedPrimary: ({ theme }) => {
-      return {
+    containedPrimary: ({ theme }) => ({
+      background: blue[500],
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      color: theme.palette.common.white,
+      '&:disabled': {
+        opacity: 0.5,
         background: blue[500],
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         color: theme.palette.common.white,
-        '&:disabled': {
-          opacity: 0.5,
-          background: blue[500],
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          color: theme.palette.common.white,
-        },
-        '&:focus': {
-          background: blue[600],
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          color: theme.palette.common.white,
-        },
-      }
-    },
+      },
+      '&:focus': {
+        background: blue[600],
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // color: theme.palette.common.white,
+      },
+    }),
+    outlinedPrimary: () => ({
+      color: grey['A700'],
+      '&:hover': {
+        background: blue[200],
+      },
+    }),
   },
   variants: [
     // size
@@ -32,11 +36,25 @@ export const MuiButton: Components['MuiButton'] = {
       props: { size: 'large' },
       style: {
         borderRadius: '28px',
-        width: '280px',
+        minWidth: '280px',
         height: '56px',
         padding: '16px 24px',
         letterSpacing: '0.1rem',
         fontSize: '16px',
+        fontWeight: 'bold',
+      },
+    },
+    {
+      props: { size: 'small' },
+      style: {
+        boxSizing: 'border-box',
+        borderRadius: '19px',
+        minWidth: '98px',
+        width: 'max-content',
+        height: '38px',
+        padding: '16px 16px',
+        letterSpacing: '0.1rem',
+        fontSize: '14px',
         fontWeight: 'bold',
       },
     },
