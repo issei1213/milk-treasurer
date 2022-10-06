47c312547cea01ab3409175b8adb8acd01179332
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-10-07 00:28:00 +0900

    [fix] トップレベルコンポーネントを修正

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -5,8 +5,10 @@ import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Router, useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
+
 import { app } from '~/libs/firebase'
 import { theme } from '~/theme'
+import { useApollo } from '~/libs/apolloClient'
 
 type AuthInitProps = {
   router: Router
@@ -20,6 +22,7 @@ const AuthInit: FC<AuthInitProps> = () => {
     const authChanged = onAuthStateChanged(auth, async (user) => {
       // ログインしていない場合は、ログイン画面へ遷移
       if (!user) {
+        localStorage.removeItem('token')
         await push({
           pathname: '/login',
         })
@@ -35,15 +38,11 @@ const AuthInit: FC<AuthInitProps> = () => {
 }
 
 function MyApp({ Component, pageProps, router }: AppProps) {
+  const apolloClient = useApollo(pageProps)
   const cache = new InMemoryCache()
-  const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    connectToDevTools: true,
-    cache,
-  })
 
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AuthInit router={router} />
