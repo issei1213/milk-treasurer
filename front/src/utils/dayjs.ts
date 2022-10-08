233a537df16e23bfc0d5b0a5700074e73aaa41db
issei1213 <59830008+issei1213@users.noreply.github.com>
2022-10-09 01:37:03 +0900

    [update] dayjsのフォーマットを変更

--- front/src/utils/dayjs.ts
+++ front/src/utils/dayjs.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
 
 export const formatYYYYMMDDHHmm = (value: string) => {
-    return dayjs(value).format('YYYY-MM-DD HH:mm')
+    return dayjs(value).format('YYYY-MM-DD H:mm')
 }
